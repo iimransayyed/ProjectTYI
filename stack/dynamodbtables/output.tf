82f25a9f01ab5db7dfd066f6477f1e3c8465a720
Imran Sayyed <iimransayyed@outlook.com>
2022-07-27 12:24:01 +0530

    ddb table tf

--- stack/dynamodbtables/output.tf
+++ stack/dynamodbtables/output.tf
@@ -1,4 +1,4 @@
 output "ddb_table_arn" {
-  description = "arn of the resource"
-  value       = aws_dynamodb_table.EasySurvey.arn
+  description = "arn of the resource"  
+  value = aws_dynamodb_table.EasySurvey.arn
 }
